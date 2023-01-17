4688a42a7bac3b39f525945c0a550914dd1e7026
Luciana Bossi Cardenas <lucianafbossi@gmail.com>
2023-01-17 16:06:57 +0100

    button play

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //Getting elements from html
 const logo = document.getElementById('logo');
+const buttonPlay = document.getElementById('btnPlay');
+
+//Logo animation
+const logoAnimation = setTimeout(logoUp, 2000);
+function logoUp() {
+    logo.classList.remove('logo_center');
+    logo.classList.add('logo_topLeft');
+    logo.style.width = "200px";
+    logo.style.height = "216px";
+    buttonPlay.classList.remove('display_none');
+    buttonPlay.classList.add('display_block');
+}
 
 //Applying overlay
 function overlay() {
